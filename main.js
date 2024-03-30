f638af05f228ad02d4f3a5f98e203570af2a2a59
PatrikSlat <patrik.slat@gmail.com>
2024-03-30 17:37:11 +0100

    Created book adding functionality

--- main.js
+++ main.js
@@ -2,18 +2,18 @@
 -> Get user data from form -> Create new book object -> display new object */
 
 const bookForm = document.querySelector(".book-form");
-const myLibrary = []
+const myLibrary = [];
 const noBooksRows = document.createElement("tr");
-noBooksRows.id = "no-data-row"
+noBooksRows.id = "no-data-row";
 const noBookData = document.createElement("td");
 noBookData.id = "no-data";
 const tableBody = document.querySelector("#tableBody");
-
+let flag = false;
+let counter = 0;
 /* Event listeners */
 
 bookForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    checkList();
+    e.preventDefault(); 
     const bookNameInput = document.querySelector("[name='book-name']").value;
     const bookAuthorInput = document.querySelector("[name='book-author']").value;
     const bookPagesInput = document.querySelector("[name='book-pages']").value;
@@ -22,8 +22,8 @@ bookForm.addEventListener("submit", (e) => {
     const bookStatusState = bookStatusCheckbox.checked ? true : false;
 
     const newBook = new Book(bookNameInput, bookAuthorInput, bookPagesInput, bookStatusState);
-    //console.log(bookAuthorInput, bookNameInput, bookStatus, bookPagesInput);
     appendBook(newBook);
+    checkList();
     displayBooks(myLibrary);
 });
 
@@ -43,23 +43,53 @@ function Book(bookName, bookAuthor, bookPages, bookStatus){
 
 function appendBook(book){
     myLibrary.push(book)
-    console.log(myLibrary);
 }
 
 function displayBooks(Array){
     const bookArray = Array;
-    for(let i =0; i <= bookArray.length; i++){
-        const currentBook = bookArray[i];
-        console.log(currentBook);
+    for(counter; counter < bookArray.length; counter++){
+        const currentBook = bookArray[counter];
+        currentBookName = currentBook.bookName;
+        currentBookAuthor = currentBook.bookAuthor;
+        currentBookPages = currentBook.bookPages;
+        currentBookStatus = currentBook.bookStatus;
+        const dynamicRow = document.createElement("tr");
+        dynamicRow.classList.add("appended-row");
+        dynamicRow.id=`${counter}`;
+        const tableDataName = document.createElement("td");
+        const tableDataAuthor = document.createElement("td");
+        const tableDataPages = document.createElement("td");
+        const tableDataStatus = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-btn");
+        deleteButton.id = `${counter}`;
+        deleteButton.innerHTML = "Delete"
+        tableDataName.innerHTML = currentBookName
+        tableDataAuthor.innerHTML = currentBookAuthor
+        tableDataPages.innerHTML = currentBookPages
+        tableDataStatus.innerHTML = currentBookStatus
+        tableBody.append(dynamicRow);
+        dynamicRow.append(tableDataName, tableDataAuthor, tableDataPages, tableDataStatus, deleteButton);
+    
+        /*
+        for(let j=0; j < 7; j++){
+            const tableData = document.createElement("td");
+            tableData.id = `${i}`;
+            dynamicRow.append(tableData);
+            tableBody.append(dynamicRow);
+        };*/
     };
 }
 
 function checkList(){
-    if(myLibrary.length === 0){
-        noBookData.innerHTML = "Curiouser and curiouser! Looks like this list is<br> still waiting for its adventures to begin.<br> Ready to take the plunge down the<br> rabbit hole and add your first book?"
-        noBooksRows.append(noBookData);
-        tableBody.append(noBooksRows);
-    }else{
-        tableBody.removeChild(noBooksRows)
+    if(flag == false){
+        if(myLibrary.length === 0){
+            noBookData.innerHTML = "Curiouser and curiouser! Looks like this list is<br> still waiting for its adventures to begin.<br> Ready to take the plunge down the<br> rabbit hole and add your first book?";
+            noBooksRows.append(noBookData);
+            tableBody.append(noBooksRows);
+        }else{
+            flag = true;
+            tableBody.removeChild(noBooksRows);
+        };
     };
 }
